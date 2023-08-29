041eccf9dbde6461a5a1433e673328643d290709
undefined <undefined@undefined.com>
2023-08-29 19:47:07 +0800

    test

--- shell.py
+++ shell.py
@@ -1,6 +1,26 @@
-def main():
-    # print(requests.get('https://ip.useragentinfo.com/json').text)
-    print('aaa')
+from jmcomic import *
 
-if __name__ == '__main__':
-    main()
+domain_ls = JmModuleConfig.get_jmcomic_domain_all()
+option = JmOption.default()
+
+status_list = {}
+
+
+def test_domain(domain: str):
+    client: JmHtmlClient = option.new_jm_client()
+    client.domain_list = [domain]
+    msg = 'ok'
+
+    try:
+        client.get_album_detail('123456')
+    except Exception as e:
+        msg = str(e.args)
+        pass
+
+    status_list['domain'] = msg
+
+
+multi_thread_launcher(
+    iter_objs=domain_ls,
+    apply_each_obj_func=test_domain,
+)
