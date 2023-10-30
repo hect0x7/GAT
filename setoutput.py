e5342b506b672e6f77e371061dfe466af6d9f76b
hect0x7 <undefined@undefined.com>
2023-10-30 20:39:22 +0800

    v2.2.2: 123;123;123;123

--- setoutput.py
+++ setoutput.py
@@ -1,6 +1,19 @@
 from common import *
 
 
+def parse_body(body):
+    if ';' not in body:
+        return body
+
+    parts = body.split(";")
+    points = []
+    for i, e in enumerate(parts):
+        e: str = e.strip()
+        points.append(f'{i}. {e}')
+
+    return '\\n'.join(points)
+
+
 def add_output(k, v):
     os.system(f'echo {k}={v} >> $GITHUB_OUTPUT')
 
@@ -13,4 +26,4 @@ assert match is not None, f'format is wrong: {commit_message}'
 tag, body = match[1], match[2]
 
 add_output('tag', tag)
-add_output('body', body)
+add_output('body', parse_body(body))
